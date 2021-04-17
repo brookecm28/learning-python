1f90f9b12251b0f4402455c3754138061396c410
Brooke Miller <brookecm28@gmail.com>
2021-04-17 16:09:50 -0600

    variables

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,5 @@
 # to print in terminal, command: py nameOfFile.py
+#python uses lowercase only, no camelCase
 
 #to print
 print("this will be printed")
@@ -7,3 +8,15 @@ print("this will be printed")
 x = 1
 if x == 1: 
     print("x is", x)
+
+#floats can be defined in one of two ways:
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+
+#strings can be defined by single or double quotation marks; double quotations allow you to use single quotation marks within the string
+mystring = 'hello'
+print(mystring)
+mynewstring = "hello"
+print(mynewstring)
