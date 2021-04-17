70401382556fdb24d92323eccf6ed145ecd94b0f
Brooke Miller <brookecm28@gmail.com>
2021-04-16 19:10:49 -0600

    initial commit

---
+++ helloWorld.py
@@ -0,0 +1,9 @@
+# to print in terminal, command: py nameOfFile.py
+
+#to print
+print("this will be printed")
+
+# four spaces is one indent, which is equivalent to {} in JS
+x = 1
+if x == 1: 
+    print("x is", x)
