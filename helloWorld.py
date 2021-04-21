534e7a0e77469b2cf70a3db8cf05b897454868f9
Brooke Miller <brookecm28@gmail.com>
2021-04-21 09:26:05 -0600

    for loop info

--- helloWorld.py
+++ helloWorld.py
@@ -10,7 +10,7 @@ if x == 1:
     print("x is", x)
 
 #floats can be defined in one of two ways:
-myfloat = 7.0
+myfloat = 7.3
 print(myfloat)
 myfloat = float(7)
 print(myfloat)
@@ -18,5 +18,22 @@ print(myfloat)
 #strings can be defined by single or double quotation marks; double quotations allow you to use single quotation marks within the string
 mystring = 'hello'
 print(mystring)
-mynewstring = "hello"
+mynewstring = "hello won't do"
 print(mynewstring)
+
+#lists are similar to arrays; can contain any type and number of variables
+mylist = []
+#use .append to add to an existing list
+mylist.append(1)
+mylist.append(2)
+mylist.append(4)
+
+#for loop utililzing lists
+for x in mylist:
+    if x == 2:
+        print(x)
+    else:
+        print("nope")
+
+#list data is accessed via index, exactly like arrays:
+print(mylist[0])
